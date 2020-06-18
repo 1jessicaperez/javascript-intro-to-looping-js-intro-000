2e390481440e82a452ead7bbfd66c2f16ef1dad7
Jessica Perez <1jessicaperez@gmail.com>
2020-06-18 06:12:12 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,31 @@
+function forLoop (array){
+  for (let i = 0; i < 25; i++){
+    if (i === 1){
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push("I am ${i} strange loop.")
+    }
+  }
+  return array;
+}
+
+function whileLoop (n){
+  let countdown = n;
+  while (n > 0){
+    console.log(countdown[n])
+    countdown -= 1
+  }
+  return "done";
+}
+
+let i = 0;
+function doWhile(3){
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+
+do {
+  console.log("I run once regardless");
+} while (incrementVariable() < 3);
+}
